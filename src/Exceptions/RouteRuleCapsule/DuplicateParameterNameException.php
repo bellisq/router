9b66b0e9dda5af5108853fc7d0ffd82623046728
Showsay You <akizuki.c10.l65@gmail.com>
2018-02-05 14:10:31 +0900

    Implement RouteRuleCapsule

---
+++ src/Exceptions/RouteRuleCapsule/DuplicateParameterNameException.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Bellisq\Router\Exceptions\RouteRuleCapsule;
+
+use LogicException;
+
+
+/**
+ * [Exception] Duplicate Parameter Name
+ *
+ * @author Showsay You <akizuki.c10.l65@gmail.com>
+ * @copyright 2018 Bellisq. All Rights Reserved.
+ * @package bellisq/router
+ * @since 1.0.0
+ */
+class DuplicateParameterNameException
+    extends LogicException
+{
+
+}
---
+++ src/Exceptions/RouteRuleCapsule/InvalidConstraintException.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Bellisq\Router\Exceptions\RouteRuleCapsule;
+
+use LogicException;
+
+
+/**
+ * [Exception] Invalid Constraint
+ *
+ * No such parameter found in the rule.
+ *
+ * @author Showsay You <akizuki.c10.l65@gmail.com>
+ * @copyright 2018 Bellisq. All Rights Reserved.
+ * @package bellisq/router
+ * @since 1.0.0
+ */
+class InvalidConstraintException
+    extends LogicException
+{
+
+}
